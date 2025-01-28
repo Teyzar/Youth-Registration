4660e4277eb10a5e0a87be50c1e21cb33d52a0ba
Thomas Andrew Zaragoza <thomasandrewzaragoza@gmail.com>
2025-01-29 00:54:52 +0800

    update connection to supabase

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,6 +4,6 @@ import { NextRequest } from "next/server"
 
 export async function POST(req: NextRequest) {
     const { name, email, phone, message } = await req.json();
-    const { data, error } = await supabase.from('messages').insert({ name, email, phone, message });
+    const { data, error } = await supabase.from('youth').insert({ name, email, phone, message });
     return NextResponse.json({ data, error });
 }
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseUrl = process.env.SUPABASE_URL
 if (!supabaseUrl) throw new Error('Missing NEXT_PUBLIC_SUPABASE_URL environment variable')
 
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseKey = process.env.SUPABASE_ANON_KEY
 if (!supabaseKey) throw new Error('Missing NEXT_PUBLIC_SUPABASE_ANON_KEY environment variable')
 
 const supabase = createClient(supabaseUrl, supabaseKey)
-
-console.log(supabaseUrl, supabaseKey)
 
 export default supabase;
